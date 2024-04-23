0702fdaee86cb8fc563a2f5d5215ce8c0a9c8a56
liyarafika <liyarafika1@gmail.com>
2024-04-23 19:57:49 +0700

    UTS_230441100145_LiyaRafikasari

---
+++ Uts/uts/pkg1/Main/UTS.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uts.pkg1.Main;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class UTS {
+    class mhs {
+        String Nama;
+        int Nim;
+        String Asal sekolah;
+        int Tanggal  lahir;
+        String Prodi;
+        String Fakultas;
+    }
+public class mhs({String Nama, int Nim, String Asal sekolah, int Tanggal lahir, String Prodi, String Fakultas});
+    this.Nama;
+    this.Nim;
+    this.Asal sekolah;
+    this.Tanggal lahir;
+    this.Prodi;
+    this.Fakultas;
+}
+public static void main(String[] args) {
+    Scanner = new scanner
+    System.out.println("Nama : ");
+    System.out.println("Nim : ");
+    System.out.println("Asal sekolah : ");
+    System.out.println("Tanggal lahir : ");
+    System.out.println("Prodi : ");
+    System.out.println("Fakultas : ");
+    }
+}
