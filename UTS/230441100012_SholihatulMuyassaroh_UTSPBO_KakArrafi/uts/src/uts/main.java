c4ab81931d8164cdb8510e7928611c370db6d8cf
lili294 <lilikarif355@gmail.com>
2024-04-23 19:10:04 +0700

    230441100012_SholihatulMuyassaroh_UTSPBO_KakArrafi

---
+++ UTS/230441100012_SholihatulMuyassaroh_UTSPBO_KakArrafi/uts/src/uts/main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uts;
+
+/**
+ *
+ * @author User
+ */
+
+import java.utill.Scanner;
+import java.utill.ArrayList;
+        
+public class main {
+    public static void main(String[] args) {
+        Scanner data = new Scanner (System.in);
+        ArrayList<mahasiswa
+    
+}
