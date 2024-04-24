3427bb576085d9e89e09240e9ea4111f464fa647
Y******** <dimasfebrian54@gmail.com>
2024-04-24 11:26:07 +0700

    UTS-230441100035-MUHAMAD DIMAS FEBRIAN-ARRAFI

---
+++ UTS-230441100035-MUHAMAD DIMAS FEBRIAN-Arrafi/uts/src/uts/Uts.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uts;
+
+import java.util.Scanner;
+ 
+     
+ 
+public class Uts {
+    
+    class data{
+        String nama;
+        String nim;
+        String asalsekolah;
+        String ttl;
+        String prodi;
+        String alamat;
+        
+    public static  tampilan{
+        
+        System.out.println("Data Mahasiswa");
+        System.out.println("Nama : " + nama);
+        
+    }
+    
+}
+    public static void main(String[] args) {
+        System.out.println("Masukkan Namanya : ");
+        nama = input.nextLine;
+        System.out.println("Masukkan NIM : ");
+        nim = input.nextLine;
+        System.out.println("Masukkan Asal Sekolah Anda :");
+        asalsekolah = input.nextLine;
+        System.out.println("Masukkan Tempat Tanggal Lahir :");
+        ttl = input.nextLine;
+        System.out.println("Masukkan Prodi : ");
+        System.out.println("SI : SISTEM INFORMASI");
+        System.out.println("TIF : TEKNIK INFORMATIKA");
+        System.out.println("TI : TEKNIK INDUSTRI");
+        System.out.println("TM : TEKNIK MESIN");
+        System.out.println("TMK : TEKNIK MEKATRONIKA");
+        System.out.println("TE : TEKNIK ELEKTRO");
+        prodi = input.nextLine;
+        System.out.println("Masukan alamat anda : ");
+        alamat = input.nextLine;
+        
+        if ttl == "SI"{
+            data.tampilan
+        }
+        else if
+            
+        
+    }
+    
+}
