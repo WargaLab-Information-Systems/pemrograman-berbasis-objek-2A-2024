e4efeff96e2728d4ce6147da168665b9f36d8c03
faizyyn <faiss8151@gmail.com>
2024-04-24 11:33:48 +0700

    UTS-230441100050_MuhammadMiftahulFaizin_arrafi

---
+++ UTS-230441100050_MUHAMMAD_MIFTAHUL_FAIZIN/uts/src/uts/Uts.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uts;
+
+/**
+ *
+ * @author Asus
+ */
+public class Uts {
+    
+    class univ{
+       String nama;    
+       long nim;
+       String sekolah;
+       long  ttl;
+       String prodi;
+       String alamat;
+     
+    public class unive{
+       static void main(String nama,long nim)
+          System.outprintln("Masukkan Nama  : " + nama)
+               nama = input.nextline;
+          System.out.println("Masukkan Nim : " + nim)
+               nim = input.nextline;
+          System.out.println(" ")
+          
+       }
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args, String sekolah,long ttl){
+        System.out.println("Masukkan Nama Sekolah : ")
+                sekolah = input.nextLine;
+        System.out.println("Masukkan ttl : ")
+                ttl = input.nextLine;
+        System.out.println("Masukkan Prodi :");
+                 prodi=input.nextLine;
+        System.out.println(" Si = Sistem Informasi ");
+        System.out.println("TIF = Teknik Informatika");
+        System.out.println("Ti = Teknik Industri");
+        System.out.println("Tm = Teknik Mesin ");
+        System.out.println("SMK = Teknik Mekatronika ");
+        System.out.println("TE = Teknik Elektro"); 
+        
+        System.out.println("Masukkan Alamat : ");
+        Alamat = input.nextLine;
+  
+                
+              
+    }
+
+}
