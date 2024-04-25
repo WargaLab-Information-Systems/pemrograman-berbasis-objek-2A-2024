b17a8537e5cb9dafdd5f6dc3c4c0c418ea1f4442
Harrysvn <bgherry14@gmail.com>
2024-04-26 05:02:56 +0700

    230441100132_HERIYANTO_Modul3_KakRizqy

---
+++ UTS/UTS-230441100132-HERIYANTO/UTS/src/main/java/UTS_PBO/DataMahasiswa.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UTS_PBO;
+
+public class DataMahasiswa {
+    String Nama;
+    Integer Nim;
+    String AsalSekolah;
+    Integer TanggalLahir;
+    String Alamat;
+    
+    public void DataMahasiswa(String nama,Integer nim,String asalsekolah,Integer tanggallahir,String alamat){
+        Nama = nama;
+        Nim = nim;
+        AsalSekolah = asalsekolah;
+        TanggalLahir = tanggallahir;
+        Alamat = alamat;
+    }
+}   
