b49bcdcb6e586721b35704460db0aac55541c49e
noerkhofifah2003@gmail.com <noerkhofifah2003@gmail.com>
2024-04-27 13:44:55 +0700

    230441100190-NoerKhofifahOktaviaYusuf-kak ardi

---
+++ UTS/UTS-230441100190-NOER KHOFIFAH OKTAVIA YUSUF/src/tgs/uts/TgsUts.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tgs.uts;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Al Fariz
+ */
+public class TgsUts {
+    String nama;
+    long Nim ;
+    String AsalSekolah;
+    String TanggalLahir;
+    String Prodi ;
+    String Alamat;
+    
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System out.println ("nama mahasiswa : " + nama);
+        System out.printlong ("nim mahasiswa : " + Nim );
+        System out.println ("Asal sekolah mahasiwa : " + AsalSekolah);
+        Sytem out.println ("Tanggal lahir mahasiswa : " + TanggalLahir);
+        System out.println ("Prodi mahasiswa : " + Prodi);
+        System out.println ("Alamat mahasiswa : " + Alamat);
+                
+        // TODO code application logic here
+    }
+    
+}
