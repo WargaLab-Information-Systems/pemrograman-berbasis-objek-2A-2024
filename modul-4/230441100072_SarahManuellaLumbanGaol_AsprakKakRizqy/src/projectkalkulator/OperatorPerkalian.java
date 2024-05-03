a961bb12dc61e9b14c2e587405b7061d6da02dbc
sarahmnuella <sarahmaneullalumbangaol21@gmail.com>
2024-05-03 21:22:17 +0700

    230441100072_SarahManuellaLumbanGaol_Modul4_AsprakKakRizqy

---
+++ modul-4/230441100072_SarahManuellaLumbanGaol_AsprakKakRizqy/src/projectkalkulator/OperatorPerkalian.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectkalkulator;
+
+/**
+ *
+ * @author USER
+ */
+public class OperatorPerkalian extends Kalkulator {
+
+    @Override
+    public void tampil() {
+        double hasil=getA()*getB();
+        System.out.println("Hasil Perkalian adalah "+hasil);
+    }
+    
+}
