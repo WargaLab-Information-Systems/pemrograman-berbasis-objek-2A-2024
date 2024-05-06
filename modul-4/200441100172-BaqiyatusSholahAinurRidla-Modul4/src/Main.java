248aafa7ee32d7504c9a2cf847387eae5f12b031
BaqiyatusSholah <baqiyatussholahar@gmail.com>
2024-05-06 15:41:04 +0700

    200441100172-BaqiyatusSholahAinurRidla-Modul4-Arrafi

---
+++ modul-4/200441100172-BaqiyatusSholahAinurRidla-Modul4/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        OperasiPenjumlahan penjumlahan = new OperasiPenjumlahan();
+        OperasiPengurangan pengurangan = new OperasiPengurangan();
+        OperasiPerkalian perkalian = new OperasiPerkalian();
+        OperasiPembagian pembagian = new OperasiPembagian();
+        penjumlahan.setA(9.25);
+        penjumlahan.setB(1.25);
+        penjumlahan.setC();
+        penjumlahan.getA();
+        penjumlahan.getB();
+        penjumlahan.getC();
+        penjumlahan.Tampil();
+
+        pengurangan.setA(9.25);
+        pengurangan.setB(1.25);
+        pengurangan.setC();
+        pengurangan.getA();
+        pengurangan.getB();
+        pengurangan.getC();
+        pengurangan.Tampil();
+
+        perkalian.setA(9.25);
+        perkalian.setB(1.25);
+        perkalian.setC();
+        perkalian.getA();
+        perkalian.getB();
+        perkalian.getC();
+        perkalian.Tampil();
+
+        pembagian.setA(9.25);
+        pembagian.setB(1.25);
+        pembagian.setC();
+        pembagian.getA();
+        pembagian.getB();
+        pembagian.getC();
+        pembagian.Tampil();
+
+    }
+}
