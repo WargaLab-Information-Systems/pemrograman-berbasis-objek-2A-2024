be1f244ba2827a186ef9fd0d93ddc8b46289d1b9
izza26 <izzafegianti@gmail.com>
2024-05-08 09:42:54 +0700

    230441100180-ArhamizFegianti-Modul4-KakArdi

---
+++ modul-4/230441100180-ArhamizFegianti/src/project4/Penjumlahan.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project4;
+
+/**
+ *
+ * @author Amira Alissiya
+ */
+public class Penjumlahan extends Kalkulator{
+    @Override
+    public void tampil() {
+        double hasil=getA() + getB( );
+        System.out.println("Hasil Penjumlahan adalah  " + hasil);
+}
+
+}
