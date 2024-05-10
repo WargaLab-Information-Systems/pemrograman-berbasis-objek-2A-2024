8df026093dacf7b81c106ba0679898220a0a6ccf
BaqiyatusSholah <baqiyatussholahar@gmail.com>
2024-05-10 17:25:58 +0700

    200441100172-BaqiyatusSholahAinurRidla-Modul5-Arrafi

---
+++ modul-5/200441100172-BaqiyatusSholahAinurRidla-Modul5/src/OperasiBilangan.java
@@ -0,0 +1,13 @@
+public abstract class OperasiBilangan {
+    protected double a, b, c, d;
+
+    protected abstract void setA(double a);
+    protected abstract void setB(double b);
+    protected abstract void setC(double c);
+    protected abstract void setD();
+    protected abstract double getA();
+    protected abstract double getB();
+    protected abstract double getC();
+    protected abstract double getD();
+    protected abstract void Tampil();
+}
---
+++ modul-5/200441100172-BaqiyatusSholahAinurRidla-Modul5/src/OperasiBilanganCetak.java
@@ -0,0 +1,10 @@
+public class OperasiBilanganCetak {
+
+    private void CetakSemua(OperasiBilangan OB, double a, double b, double c){
+        OB.setA(a);
+        OB.setB(b);
+        OB.setC(c);
+        OB.setD();
+        OB.Tampil();
+    }
+}
