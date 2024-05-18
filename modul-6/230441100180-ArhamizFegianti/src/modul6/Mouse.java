dd18f3f1bd7177733f355127624fd2af6008f77c
izza26 <izzafegianti@gmail.com>
2024-05-18 11:32:19 +0700

    230441100180-ArhamizFegianti-KakArdi

---
+++ modul-6/230441100180-ArhamizFegianti/src/modul6/Mouse.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package modul6;
+
+/**
+ *
+ * @author Amira Alissiya
+ */
+public interface Mouse {
+    public void klik_kanan();
+    public void klik_kiri();
+}
