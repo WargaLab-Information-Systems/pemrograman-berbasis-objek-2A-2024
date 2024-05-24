682e2a3dedb126958321c30feb24cc6b0285fa5f
zaxllpjm <ameliazendia123@gmail.com>
2024-05-24 13:17:20 +0700

    230441100015_ZendiaAmelia_Modul6_Arrafi

---
+++ modul-6/KomputerCetak.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.main;
+
+/**
+ *
+ * @author ZENDIA AMELIA
+ */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class KomputerCetak {
+      public void cetak(List<Komputer> obj) {
+        for (Komputer komputer : obj) {
+            komputer.cetak_data();  
+        }
+     }
+      
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+        KomputerCetak cetak = new KomputerCetak();
+        List<Komputer> komputerList = new ArrayList<>();
+
+        System.out.println("Masukkan jumlah jenis komputer: ");
+        int jumlah = scanner.nextInt();
+        scanner.nextLine(); 
+        
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("Masukkan jenis komputer yang dipilih (PC/Laptop/Notebook):");
+            String type = scanner.nextLine();
+            Komputer komputer = cetak.JenisKomputer(type);
+            if (komputer != null) {
+                komputerList.add(komputer);
+            } else {
+                System.out.println("Silakan coba lagi.");
+                i--; 
+            }
+        }
+        
+       for (Komputer komputer : komputerList) {
+            System.out.println("Pilih Operation " + komputer.getClass().getSimpleName() + " :");
+            System.out.println("1. Hidupkan OS");
+            System.out.println("2. Matikan OS");
+            System.out.println("3. Klik Kanan");
+            System.out.println("4. Klik Kiri");
+            System.out.println("5. Tekan Enter");
+            System.out.println("6. Cetak Data");
+            System.out.print("Masukkan pilihan (1 sampai 6): ");
+            int pilihan = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch (pilihan) {
+                case 1 -> komputer.hidupkan_os();
+                case 2 -> komputer.matikan_os();
+                case 3 -> komputer.klik_kanan();
+                case 4 -> komputer.klik_kiri();
+                case 5 -> komputer.tekan_enter();
+                case 6 -> komputer.cetak_data();
+                default -> System.out.println("Pilihan tidak valid.");
+            }
+        }
+    }
+    
+        public Komputer JenisKomputer(String type) {
+        return switch (type.toLowerCase()) {
+            case "pc" -> new PC();
+            case "laptop" -> new Laptop();
+            case "notebook" -> new Netbook();
+            default -> null;
+        };
+    }
+}
