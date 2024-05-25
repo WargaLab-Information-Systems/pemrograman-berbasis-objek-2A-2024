dd3a01dadaa841e9bd8121b311e5b85d2175c360
DylaNSWER06 <akunsmurfh1122@gmail.com>
2024-05-25 15:19:00 +0700

    230441100027_DylanAkhtarreza_Modul5_Arrafi

---
+++ modul-5/pbomodul5/polimorfisme/src/polimorfisme/OperasiPenjumlahan.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package polimorfisme;
+
+/**
+ *
+ * @author USER
+ */
+class OperasiPenjumlahan extends OperasiBilangan {
+    double D;
+    // Overloading set_A untuk menerima bilangan bulat
+    protected void set_A(int a) {
+        A = (double) a; // Konversi bilangan int menjadi double
+    }
+    
+    @Override
+    protected void set_A(double a) {
+        A = a;
+    }
+
+    protected void set_B(int b) {
+        B = (double) b;
+    }
+
+    @Override
+    protected void set_B(double b) {
+        B = b;
+    }
+
+    protected void set_C(int c) {
+        C = (double) c;
+    }
+
+    @Override
+    protected void set_C(double c) {
+        C = c;
+    }
+
+    @Override
+    protected void set_D() {
+        D = A + B + C;
+    }
+
+    @Override
+    protected double get_A() {
+        return A;
+    }
+
+    @Override
+    protected double get_B() {
+        return B;
+    }
+
+    @Override
+    protected double get_C() {
+        return C;
+    }
+
+    @Override
+    protected double get_D() {
+        return D;
+    }
+
+    @Override
+    protected void tampil() {
+        System.out.println("Hasil penjumlahan " + get_A() + " + " + get_B() + " + " + get_C()+ " = " + get_D());
+    }
+
+}
