e9e918e64854553f01bbacfd7b05da5bbfbfe4f7
MaghfiraRahmadani <pasarkuebkl@gmail.com>
2024-05-27 18:36:10 +0700

    230441100001_MaghfiraRahmadaniHisbulwaton_Modul6_KakArrafi

---
+++ modul-6/230441100001_Maghfira Rahmadani Hisbulwaton_Modul6_KakArrafi/pbo6/src/pbo6/Komputer.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo6;
+
+/**
+ *
+ * @author asus
+ */
+abstract class Komputer implements Mouse, Keyboard, Printer {
+   
+    abstract void hidupkan_OS();
+    abstract void matikan_OS();
+}
---
+++ modul-6/230441100001_Maghfira Rahmadani Hisbulwaton_Modul6_KakArrafi/pbo6/src/pbo6/Mouse.java
@@ -0,0 +1,16 @@
+package pbo6;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author asus
+ */
+public interface Mouse {
+    void klik_kanan();
+    void klik_kiri ();
+    
+}
