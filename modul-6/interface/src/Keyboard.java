10b40a4158ef95ba6e2bdb5ba7dad1499cafc351
faizyyn <faiss8151@gmail.com>
2024-06-11 04:51:20 +0700

    230441100050_MuhammadMiftahulFaizin_Modul6_ARRAFI

---
+++ modul-6/interface/src/Keyboard.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+// Interface untuk Keyboard
+public interface Keyboard {
+    void tekan_enter();
+}
---
+++ modul-6/interface/src/Mouse.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+// Interface untuk Mouse
+public interface Mouse {
+    void klik_kanan();
+    void klik_kiri();
+}
