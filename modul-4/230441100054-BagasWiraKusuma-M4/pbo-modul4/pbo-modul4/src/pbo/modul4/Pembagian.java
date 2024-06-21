8358356f9ef4688edb21090d98476ea83c017c1b
Baswiraa <bagaswrkm@gmail.com>
2024-06-22 00:24:34 +0700

    230441100054-BagasWiraKusuma-M4-KakRizky

---
+++ modul-4/230441100054-BagasWiraKusuma-M4/pbo-modul4/pbo-modul4/src/pbo/modul4/Pembagian.java
@@ -0,0 +1,40 @@
+package pbo.modul4;
+
+class Pembagian extends OperasiMatematika {
+    @Override
+    protected void setA(double a) {
+        this.a = a;
+    }
+
+    @Override
+    protected void setB(double b) {
+        this.b = b;
+    }
+
+    @Override
+    protected void setC() {
+        this.c = this.a / this.b;
+    }
+
+    @Override
+    protected double getA() {
+        return this.a;
+    }
+
+    @Override
+    protected double getB() {
+        return this.b;
+    }
+    @Override
+    protected double getC() {
+        return this.c;
+    }
+
+    @Override
+    protected void tampil() {
+        System.out.println("Operasi Pembagian");
+        System.out.println("Nilai A: " + this.a);
+        System.out.println("Nilai B: " + this.b);
+        System.out.println("Hasil Nilai A/B: " + this.c);
+    }
+}
