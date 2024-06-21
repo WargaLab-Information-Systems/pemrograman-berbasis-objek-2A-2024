41180f6bd81570695a01626da54cf1f356ca9f50
Baswiraa <bagaswrkm@gmail.com>
2024-06-22 00:29:48 +0700

    230441100054-BagasWiraKusuma-M6-KakRizqy

---
+++ modul-6/230441100054-BagasWiraKusuma-M6/pbomodul-6/interface/src/com/pkg/KomputerCetak.java
@@ -0,0 +1,13 @@
+package com.pkg;
+
+public final class KomputerCetak {
+    public void cetak(Komputer k) {
+        k.hidupkan_os();
+        k.klik_kanan();
+        k.klik_kiri();
+        k.tekan_enter();
+        k.cetak_data();
+        k.matikan_os();
+        System.out.println("--------------");
+    }
+}
