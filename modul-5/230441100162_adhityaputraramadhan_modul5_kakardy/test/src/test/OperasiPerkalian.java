0c1f6883ea395b0f92e37417f2fc339a2df5ff34
veldoraaa <adhityaputra351@gmail.com>
2024-06-23 12:42:42 +0700

    230441100162_adhityaputraramadhan_modul5_kakardy

---
+++ modul-5/230441100162_adhityaputraramadhan_modul5_kakardy/test/src/test/OperasiPerkalian.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author AdhityaPutraaa
+ */
+// Class untuk operasi perkalian
+class Perkalian extends OperasiMatematika {
+    // Implementasi metode operasi untuk perkalian
+    @Override
+    public double operasi(double a, double b) {
+        return a * b;
+    }
+    
+    // Implementasi metode deskripsi untuk perkalian
+    @Override
+    public String deskripsi() {
+        return "Perkalian";
+    }
+}
