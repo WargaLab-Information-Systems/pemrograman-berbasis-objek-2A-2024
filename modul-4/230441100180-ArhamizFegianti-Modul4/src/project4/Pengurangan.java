c33c3feea4f16401db5f8b378a8468557ad2eb03
izza26 <izzafegianti@gmail.com>
2024-06-23 14:53:33 +0700

    230441100180-ArhamizFegianti-KakArdi

---
+++ modul-4/230441100180-ArhamizFegianti-Modul4/src/project4/Pengurangan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project4;
+
+/**
+ *
+ * @author Amira Alissiya
+ */
+public class Pengurangan extends Kalkulator{
+    @Override
+    public void tampil() {
+        double hasil = getA() - getB( );
+        System.out.println("Hasil Pengurangan adalah  " + hasil);
+}
+}
