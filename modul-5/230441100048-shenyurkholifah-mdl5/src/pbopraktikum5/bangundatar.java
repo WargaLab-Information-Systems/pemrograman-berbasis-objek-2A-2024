c54a2e197486732e65d53e439866ed68ae6b51dd
sshnnyklhh <shenynurkholifah.com>
2024-06-25 14:18:07 +0700

    230441100072_ShenyNurKholifah_AsprakKakArafi_Modul5

---
+++ modul-5/230441100048-shenyurkholifah-mdl5/src/pbopraktikum5/bangundatar.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbopraktikum5;
+
+/**
+ *
+ * @author sheny
+ */
+public class bangundatar {
+    
+    private double phi = 3.14;
+    protected double hasil;
+    
+//    bangundatar(int sisi){
+//        this.hasil = sisi * sisi;
+//    }
+    
+//    bangundatar(double phi, int r){
+//        this.hasil = phi *(r*r);
+//    }
+    
+//    void setHasil(double phi,int r){
+//        this.hasil = phi * (r*r);
+//    }
+    
+    double getHasil(){
+        return hasil;
+    }
+    
+    double getphi(){
+         return this.phi;
+}
+}
---
+++ modul-5/230441100048-shenyurkholifah-mdl5/src/pbopraktikum5/persegi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbopraktikum5;
+
+/**
+ *
+ * @author sheny
+ */
+public class persegi extends bangundatar {
+    void definisi(){
+        System.out.println("ini adalah persegi");
+    }
+    
+}
